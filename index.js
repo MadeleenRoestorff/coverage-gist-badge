38260b19fb506e63123aa3e77ba1c876b273f058
Maria Roestorff <maria.roestorff@gmail.com>
2023-01-25 16:43:53 +0000

    change gist pdate

--- index.js
+++ index.js
@@ -1,41 +1,58 @@
 const core = require('@actions/core');
-const axios = require('axios');
+const { Octokit } = require('@octokit/action');
 
-const config = {
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${core.getInput('auth')}`,
-  },
+const main = async () => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${core.getInput('auth')}`,
+      'User-Agent': 'MadeleenRoestorff',
+    },
+  };
+
+  console.log(config);
+
+  const content = {
+    schemaVersion: 1,
+    label: core.getInput('label'),
+    message: core.getInput('message'),
+  };
+
+  const color = core.getInput('color');
+  const filename = core.getInput('filename');
+  const namedLogo = core.getInput('namedLogo');
+
+  if (namedLogo != '') {
+    content.namedLogo = namedLogo;
+  }
+
+  if (color != '') {
+    content.color = color;
+  }
+
+  const request = JSON.stringify({
+    files: { [filename]: { content: JSON.stringify(content) } },
+  });
+
+  const octokit = new Octokit({
+    auth: 'YOUR-TOKEN',
+  });
+
+  const response = await octokit.gists.update({
+    gist_id: core.getInput('gistID'),
+    description: 'An updated gist description',
+    request,
+  });
+
+  const { files } = response.data;
+
+  console.log(files[filename]);
+
+  core.setOutput('content', files[filename].content);
 };
 
-console.log(config);
-
-const content = {
-  schemaVersion: 1,
-  label: core.getInput('label'),
-  message: core.getInput('message'),
-};
-
-const color = core.getInput('color');
-const filename = core.getInput('filename');
-const namedLogo = core.getInput('namedLogo');
-
-if (namedLogo != '') {
-  content.namedLogo = namedLogo;
-}
-
-if (color != '') {
-  content.color = color;
-}
-
-const request = JSON.stringify({
-  files: { [filename]: { content: JSON.stringify(content) } },
+main().catch((error) => {
+  console.error(error);
+  core.setFailed(error.message);
+  process.exit(-1);
 });
-
-axios.post(
-  `https://api.github.com/gists/${core.getInput('gistID')}/`,
-  request,
-  config
-);
-
-console.log(request);
