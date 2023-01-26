031c0e2592788cd662036ef3067d27def6f673ed
Maria Roestorff <maria.roestorff@gmail.com>
2023-01-26 14:49:17 +0000

    ignore specific node modules

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-const core = require('@actions/core');
-const { Octokit } = require('@octokit/core');
+const core = require("@actions/core");
+const { Octokit } = require("@octokit/core");
 
 const main = async () => {
   const content = {
     schemaVersion: 1,
-    label: core.getInput('label'),
-    message: core.getInput('message'),
+    label: core.getInput("label"),
+    message: core.getInput("message")
   };
 
-  const color = core.getInput('color');
-  const filename = core.getInput('filename');
-  const namedLogo = core.getInput('namedLogo');
-  const percantage = Number(core.getInput('message').replace('%', ''));
+  const color = core.getInput("color");
+  const filename = core.getInput("filename");
+  const namedLogo = core.getInput("namedLogo");
+  const percantage = Number(core.getInput("message").replace("%", ""));
 
-  if (color == '') {
+  if (color === "") {
     if (percantage >= 90) {
-      content.color = 'brightgreen';
+      content.color = "brightgreen";
     } else if (percantage >= 80) {
-      content.color = 'green';
+      content.color = "green";
     } else if (percantage >= 70) {
-      content.color = 'yellowgreen';
+      content.color = "yellowgreen";
     } else if (percantage >= 60) {
-      content.color = 'yellow';
+      content.color = "yellow";
     } else if (percantage >= 50) {
-      content.color = 'orange';
+      content.color = "orange";
     } else {
-      content.color = 'red';
+      content.color = "red";
     }
   } else {
     content.color = color;
   }
 
-  if (namedLogo != '') {
+  if (namedLogo !== "") {
     content.namedLogo = namedLogo;
   }
 
   const octokit = new Octokit({
-    auth: core.getInput('auth'),
+    auth: core.getInput("auth")
   });
 
-  const response = await octokit.request('PATCH /gists/{gist_id}', {
-    gist_id: core.getInput('gistID'),
-    description: 'An updated gist description',
+  const response = await octokit.request("PATCH /gists/{gistID}", {
+    gistID: core.getInput("gistID"),
+    description: "An updated gist description",
     files: {
       [filename]: {
-        content: JSON.stringify(content),
-      },
-    },
+        content: JSON.stringify(content)
+      }
+    }
   });
 
   console.log(response.data);
