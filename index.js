ebfa6c7dc38c9e720f770efdd68644b57bef03fe
Maria Roestorff <maria.roestorff@gmail.com>
2023-01-26 11:07:52 +0000

    added color selector debug

--- index.js
+++ index.js
@@ -13,31 +13,19 @@ const main = async () => {
   const namedLogo = core.getInput('namedLogo');
   const percantage = Number(core.getInput('message').replace('%', ''));
 
-  console.log('percantage', percantage);
-
   if (color == '') {
-    switch (percantage) {
-      case percantage >= 90:
-        content.color = 'brightgreen';
-        break;
-      case percantage >= 80:
-        content.color = 'green';
-        break;
-      case percantage >= 70:
-        content.color = 'yellowgreen';
-        break;
-      case percantage >= 60:
-        content.color = 'yellow';
-        break;
-      case percantage >= 50:
-        content.color = 'orange';
-        break;
-      case percantage < 50:
-        content.color = 'red';
-        break;
-      default:
-        content.color = 'pink';
-        break;
+    if (percantage >= 90) {
+      content.color = 'brightgreen';
+    } else if (percantage >= 80) {
+      content.color = 'green';
+    } else if (percantage >= 70) {
+      content.color = 'yellowgreen';
+    } else if (percantage >= 60) {
+      content.color = 'yellow';
+    } else if (percantage >= 50) {
+      content.color = 'orange';
+    } else {
+      content.color = 'red';
     }
   } else {
     content.color = color;
